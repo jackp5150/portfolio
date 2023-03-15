607a4e19cd01702dc3f38a280e42e9fc47c57867
Jack <jackp5150@gmail.com>
2023-03-15 16:32:48 -0500

    Update vite config for correct publicPath and assetsDir

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
   build: {
     outDir: 'dist',
-    publicPath: '/portfolio/assets/',
+    publicPath: '/portfolio/dist/assets/',
   },
 });
