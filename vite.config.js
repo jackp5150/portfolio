a507d889a208eaa5717376596972277bea2025f1
Jack <jackp5150@gmail.com>
2023-03-15 16:05:07 -0500

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import WindiCSS from 'vite-plugin-windicss';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/portfolio/',
+  plugins: [react(), WindiCSS()],
+  base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
   build: {
+    outDir: 'dist',
     publicPath: '/portfolio/assets/',
   },
 });
